9e2b6810bd0a345a608ff99a0731dfd537078f74
dough <viktor.melin@me.com>
2022-11-11 11:20:30 +0100

    refactor(frontend/dashboard): cleanup

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,34 +1,11 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { Navigate, Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import {
-	Container,
-	Box,
-	Typography,
-	TextField,
-	Button,
-	Snackbar,
-	Alert,
-	IconButton,
-	Menu,
-	MenuItem,
-	ListItemIcon,
-	Tooltip,
-} from '@mui/material';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { Container, Box } from '@mui/material';
 
-import { Person, Group, Logout, Settings } from '@mui/icons-material';
-import { useState } from 'react';
-import { addMovie, getMovie } from '../features/movies/movieSlice';
-import Notify from '../components/Notify/Notify';
 import Navbar from '../components/Navbar/Navbar';
 
 const Dashboard = () => {
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
-
-	const movieData = useSelector((state) => state.movies);
 	const userData = useSelector((state) => state.auth);
-	const { page } = useSelector((state) => state.page);
 
 	if (!userData.user) {
 		return <Navigate to='/login' />;
