c7c57420d44a6cb47a1bad20594473a5f72d7550
dough <viktor.melin@me.com>
2022-11-11 11:16:17 +0100

    feat(backend/user): reset password route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {
+	resetPassword,
 	registerUser,
 	loginUser,
 	getUser,
@@ -8,6 +9,7 @@ const protect = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/', registerUser);
+router.post('/resetpassword', resetPassword);
 router.post('/login', loginUser);
 router.get('/me', protect, getUser);
 
